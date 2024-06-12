99d0832fa46f4d4bc608743bb9071500bbe8507e
witekmikolajczak <witekmikolajczak@gmail.com>
2024-06-12 23:30:09 +0200

    Initial Commit

---
+++ TaskList/TaskListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskListApp.swift
+//  TaskList
+//
+//  Created by Witek Mikolajczak on 12/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
